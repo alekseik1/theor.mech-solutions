da35f91471e83211cd72eea6324ef376c225388b
alekseik1 <alekseik1@yandex.ru>
2018-04-19 00:51:02 +0300

    Обновил шаблон для задачи

--- task.py
+++ task.py
@@ -4,12 +4,21 @@ from pylatex.base_classes import Environment
 from pylatex.utils import italic, NoEscape
 import os
 
+task_number = ''
+
 
 class Problem(Environment):
 
     def __init__(self, number=''):
         super(Problem, self).__init__()
-        #self.arguments = [number]
+        self.arguments = [number]
+
+
+class Solution(Environment):
+
+    def __init__(self, number=''):
+        super(Solution, self).__init__()
+        self.arguments = [number]
 
 
 def fill_document(doc: Document):
@@ -27,7 +36,7 @@ def fill_preambula(doc: Document):
     @param doc: Документ, в котором заполнить
     @return:
     """
-    doc.packages = []
+    doc.packages = set()
     doc.preamble = []
     doc.documentclass = Command('documentclass', options=['12pt'], arguments=['book'])
     doc.preamble.append(NoEscape(r'\input{%s/preambula}' % os.path.abspath(os.path.join(os.getcwd(), os.pardir))))
@@ -37,4 +46,4 @@ if __name__ == '__main__':
     doc = Document('basic')
     fill_document(doc)
     fill_preambula(doc)
-    doc.generate_pdf(os.path.basename(__file__), clean_tex=False) 
+    doc.generate_pdf(task_number, clean_tex=False)
